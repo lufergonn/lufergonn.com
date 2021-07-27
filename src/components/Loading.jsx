5af14165f100373df347f4fec263855b0e27299b
Luis Fernando <lufergonn@gmail.com>
2021-07-26 21:23:27 -0500

    Loader created

---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import '../assets/css/loading.css';
+
+const loading = () => {
+  return (
+    <div className="loading__box">
+      <div className="loading">Loading...</div>
+    </div>
+  );
+}
+
+export default loading;
