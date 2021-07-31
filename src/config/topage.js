980d0a1f551936acdef45c669dea6b32b943a620
Luis Fernando <lufergonn@gmail.com>
2021-07-30 23:41:46 -0500

    Added analysis component

--- src/config/topage.js
+++ src/config/topage.js
@@ -1,7 +1,8 @@
 import Github from '../pages/Github';
+import Analysis from '../pages/Analysis';
 
 const ToPage = (page) => {
-  if (page === 'analysis') return 'analysis';
+  if (page === 'analysis') return <Analysis />;
   if (page === 'github') return <Github />;
 
   return 'home';
