20331b777a59482e477a3542177a65c142134fd4
Luis Fernando <lufergonn@gmail.com>
2021-07-31 22:26:40 -0500

    added heroCard component

---
+++ src/components/Analysis/HeroCard.jsx
@@ -0,0 +1,14 @@
+const HeroCard = ({ background, count, text }) => {
+  return (
+    <div className="column">
+      <div className={`box has-background-${background} has-text-white`}>
+        <p className='has-text-white'>
+          <span className="title is-3 is-spaced has-text-white">{count} </span>
+          <span className='subtitle is-5 has-text-white'>{text}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default HeroCard;
