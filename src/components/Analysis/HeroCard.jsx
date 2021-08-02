61f1b98f7e9260c6d8dd545c0765b0328d78ad59
Luis Fernando <lufergonn@gmail.com>
2021-08-01 19:23:11 -0500

    Updated ' --> "

--- src/components/Analysis/HeroCard.jsx
+++ src/components/Analysis/HeroCard.jsx
@@ -2,9 +2,9 @@ const HeroCard = ({ background, count, text }) => {
   return (
     <div className="column">
       <div className={`box has-background-${background} has-text-white`}>
-        <p className='has-text-white'>
+        <p className="has-text-white">
           <span className="title is-3 is-spaced has-text-white">{count} </span>
-          <span className='subtitle is-5 has-text-white'>{text}</span>
+          <span className="subtitle is-5 has-text-white">{text}</span>
         </p>
       </div>
     </div>
